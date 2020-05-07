30e11f82e693f2bd79713f6c3b38340c9f4733d1
choonsik0928 <choonsik0928@gmail.com>
2020-05-07 21:17:45 +0900

    fragment action animation 추가 delay 추가

--- app/src/main/java/com/choonsik/blockchainwallet/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/choonsik/blockchainwallet/ui/splash/SplashFragment.kt
@@ -2,8 +2,10 @@ package com.choonsik.blockchainwallet.ui.splash
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.findNavController
 import com.choonsik.blockchainwallet.R
 import com.choonsik.blockchainwallet.base.BaseFragment
+import com.choonsik.blockchainwallet.common.EventObserver
 import com.choonsik.blockchainwallet.databinding.SplashFragmentBinding
 
 class SplashFragment : BaseFragment<SplashViewModel, SplashFragmentBinding>(
@@ -14,5 +16,13 @@ class SplashFragment : BaseFragment<SplashViewModel, SplashFragmentBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.checkSecureSetting()
+        setObserver()
+    }
+
+    private fun setObserver() {
+        viewModel.actionPinCodeRegistration.observe(viewLifecycleOwner, EventObserver {
+            val direction = SplashFragmentDirections.actionSplashToPinCodeReg()
+            binding.root.findNavController().navigate(direction)
+        })
     }
 }
--- app/src/main/java/com/choonsik/blockchainwallet/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/choonsik/blockchainwallet/ui/splash/SplashViewModel.kt
@@ -1,20 +1,30 @@
 package com.choonsik.blockchainwallet.ui.splash
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.choonsik.blockchainwallet.common.Event
 import com.choonsik.blockchainwallet.repository.AppRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SplashViewModel @Inject constructor(
     private val appRepository: AppRepository
 ): ViewModel(){
+    val actionPinCodeRegistration = MutableLiveData<Event<Unit>>()
 
     fun checkSecureSetting(){
         viewModelScope.launch {
-            Log.e("test","SecurityData = ${ appRepository.hasSecurityData()}")
+            if(!appRepository.hasSecurityData()){
+                delay(DELAY_MILLISECONDS)
+                actionPinCodeRegistration.value = Event(Unit)
+            }
         }
+    }
 
+    companion object{
+        const val DELAY_MILLISECONDS = 1000L
     }
 }
